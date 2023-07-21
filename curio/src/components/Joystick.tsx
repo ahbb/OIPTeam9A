1166d8b453c98d87a7225159d0b5c1606463cb81
bolo56moto <bolo56moto1@gmail.com>
2023-07-21 22:07:24 +0800

    Load Dynamic Question List

--- curio/src/components/Joystick.tsx
+++ curio/src/components/Joystick.tsx
@@ -3,7 +3,7 @@ import { Joystick } from "react-joystick-component";
 import { Box, Button, Container, Stack } from "@mui/material";
 import { Curio } from "../services/curioServices";
 import { DataType, PeerData } from "../services/types";
-
+import * as fs from 'fs';
 type Props = {
 	sendMessage: ((message: PeerData) => void) | undefined;
 };
@@ -17,31 +17,65 @@ export default function JoystickControlle({ sendMessage }: Props) {
 	const [question, setQuestion] = useState<string | null>(null);
 	const [answers, setAnswers] = useState<string[]>([]);
 	const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+	const [correctAnswer, setCorrectAnswer] = useState<string | null>(null);
+	const [answerCorrect, setAnswerCorrect] = useState<boolean | null>(null);
+	const [buttonsDisabled, setButtonsDisabled] = useState<boolean>(false);
 
 	const curio = new Curio();
-
+	
+	useEffect(() => {
+		if (buttonsDisabled) {
+			// Re-enable the buttons after 5 seconds
+			const timeout = setTimeout(() => setButtonsDisabled(false), 5000);
+			
+			// Clear the timeout if the component unmounts
+			return () => clearTimeout(timeout);
+		}
+	}, [buttonsDisabled]);
+	
 	// Initialize the multiple-choice question
-	const initQuestion = () => {
-	const numbers = [1, 5, 4, 8, 9, 100, 5, 78, 37, 52];
-	const correctAnswer = "100";
-	const answers = ["100", "78", "1", "8"];
+	const initQuestion = async () => {
+		try {
+			const response = await fetch('./questions.txt');
+			const text = await response.text();
+			console.log("gretg - " + text)
+			const lines = text.split('\n');
+			const randomLine = lines[Math.floor(Math.random() * lines.length)];
+			const data = randomLine.split('|');
 
-	setQuestion(`numbers = [${numbers}]\n What would be the output of this code? print(numbers[5])`);
-	setAnswers(answers);
-	setSelectedAnswer(null);
+			const question = data[0];
+			const correctAnswer = data[1];
+			const wrongAnswers = data.slice(2);
+			const answers = [correctAnswer, ...wrongAnswers].sort(() => Math.random() - 0.5);
+
+			setQuestion(question);
+			setCorrectAnswer(correctAnswer);
+			setAnswers(answers);
+						
+		} catch (err) {
+			console.error('Failed to fetch questions', err);
+		}
 	};
 
-	// Handle answer selection
-	const handleAnswerSelect = (answer: string) => {
-	setSelectedAnswer(answer);
-	if (answer === "100") {
-		curio.setParameters(0,1,100)
-		curio.move()
-	} else {
-		// Show a notification or message to indicate the wrong answer
-		// Robot will not move if the wrong answer is selected
-	}
+
+	/// Handle answer selection
+	const handleAnswerSelect = async (answer: string) => {
+		setSelectedAnswer(answer);
+		if (answer === correctAnswer) {
+			curio.setParameters(0, 1, 100);
+			curio.move();
+			setAnswerCorrect(true);
+			await initQuestion(); // load the next question
+		} else {
+			// Show a notification or message to indicate the wrong answer
+			// Robot will not move if the wrong answer is selected
+			setAnswerCorrect(false);
+			setButtonsDisabled(true); // Disable buttons when the answer is wrong
+		}
 	};
+	
+	
+
 
 	const handleConnect = () => {
 		if (sendMessage) {
@@ -117,8 +151,8 @@ export default function JoystickControlle({ sendMessage }: Props) {
 					await curio.connect();
 					setIsConnected(true);
 				}
-			} 
-			catch (error) {		
+			}
+			catch (error) {
 			}
 		};
 
@@ -167,24 +201,30 @@ export default function JoystickControlle({ sendMessage }: Props) {
 
 	return (
 		<Stack direction="column" justifyContent="center" alignItems="center" spacing={20}>
-		  {/* Render the question and answers only when isConnected is true */}
-		  {isConnected && (
-			<>
-			  <Box>{question}</Box>
-			  {answers.map((answer) => (
-				<Button
-				  key={answer}
-				  onClick={() => handleAnswerSelect(answer)}
-				  variant="contained"
-				  style={{
-					backgroundColor: selectedAnswer === answer ? "green" : undefined,
-				  }}
-				>
-				  {answer}
-				</Button>
-			  ))}
-			</>
-		  )}
+			{/* Render the question and answers only when isConnected is true */}
+			{isConnected || (
+				<>
+					<Box>{question}</Box>
+					{answers.map((answer) => (
+						<Button
+							key={answer}
+							onClick={() => handleAnswerSelect(answer)}
+							disabled={isMoving || buttonsDisabled} // disable button while the robot is moving or the buttons are disabled
+							variant="contained"
+							style={{
+								backgroundColor: selectedAnswer === answer ? (answerCorrect ? 'green' : 'red') : undefined,
+							}}
+						>
+							{answer}
+						</Button>
+						
+					))}
+					{answerCorrect !== null && (
+						<Box>{answerCorrect ? 'Your last answer was correct.' : 'Your last answer was incorrect.'}</Box>
+					)}
+				</>
+			)}
+
 		</Stack>
-	  );
+	);
 }
